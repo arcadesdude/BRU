ccb5ad5f250793f40737310267eb97b4da00768b
arcadesdude <14213202+arcadesdude@users.noreply.github.com>
2018-09-16 21:46:54 -0400

    Add files via upload

--- Bloatware-Removal-Utility.bat
+++ Bloatware-Removal-Utility.bat
@@ -1,16 +1,27 @@
 @ECHO OFF
+
 net session >nul 2>&1
+
     if %errorLevel% == 0 (
-    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d *.bat
-    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d *.ps1
-    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.exe
-    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.dll
-    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.vbs
+
+        if exist BRU-uninstall-helpers\streams.exe (
+            BRU-uninstall-helpers\streams.exe /accepteula -s -d *.bat
+            BRU-uninstall-helpers\streams.exe /accepteula -s -d *.ps1
+            BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.exe
+            BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.dll
+            BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.vbs
+        )
+
 	PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -NoExit -ExecutionPolicy Bypass -WindowStyle Hidden -File ""%~dpn0.ps1""' -Verb RunAs}"
+    
     exit
+
     ) else (
+
         echo You must be logged in as a member of the Adminstrators group and right-click
         echo this batch file then "Run as Administrator" for the PowerShell script
         echo to execute properly.
+
     )    
+
 PAUSE
