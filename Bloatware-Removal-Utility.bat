7a1a79cd52d050453742618b8107698635fbc433
arcadesdude <14213202+arcadesdude@users.noreply.github.com>
2018-09-16 21:09:25 -0400

    Add files via upload

--- Bloatware-Removal-Utility.bat
+++ Bloatware-Removal-Utility.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
 net session >nul 2>&1
     if %errorLevel% == 0 (
+    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d *.bat
+    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d *.ps1
+    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.exe
+    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.dll
+    if exist BRU-uninstall-helpers\streams.exe BRU-uninstall-helpers\streams.exe /accepteula -s -d BRU-uninstall-helpers\*.vbs
 	PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -NoExit -ExecutionPolicy Bypass -WindowStyle Hidden -File ""%~dpn0.ps1""' -Verb RunAs}"
     exit
     ) else (
